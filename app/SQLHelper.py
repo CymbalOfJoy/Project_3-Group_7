adaa727937e903bcdf3402bedc1709cfdb496a3c
bryguy911 <bjm1187@outlook.com>
2025-02-13 20:59:46 -0600

    Flask Back End Update 02/13
    APP and SQL Helper are functioning as far as what I can see

--- app/SQLHelper.py
+++ app/SQLHelper.py
@@ -2,51 +2,37 @@ from sqlalchemy import create_engine, text
 import pandas as pd
 
 
-
-
-class sqlhelper():
-
+class SQLHelper:
 
     def __init__(self):
         self.engine = create_engine("sqlite:///touristtravel.sqlite")
-# Querry 1 for line chart- sorting table by the column Total_Travel_Cost
-    def querylineData(self):
-        # Create our session (link) from Python to the DB
+
+    # Query 1 for line chart - sorting table by the column Total_Travel_Cost
+    def query_line_data(self):
         conn = self.engine.connect()  # Raw SQL/Pandas
-        # Define Query
         query = text("""SELECT *
                         FROM touristtravel
                         ORDER BY Total_Travel_Cost ASC""")
         df = pd.read_sql(query, con=conn)
-
-        # Close the connection
         conn.close()
         return df
-     
-#Query 2 for Barchart, pull data from table sorting from the accomidation type 
-   def querybarData(self):
-        # Create our session (link) from Python to the DB
+
+    # Query 2 for bar chart - pulling data from table, sorting by the accommodation type
+    def query_bar_data(self):
         conn = self.engine.connect()  # Raw SQL/Pandas
-        # Define Query
         query = text("""SELECT *
                         FROM touristtravel
-                        ORDER BY """)
+                        ORDER BY Mode_of_Travel""")
         df = pd.read_sql(query, con=conn)
-
-        # Close the connection
         conn.close()
-        return 
+        return df
 
-#Query 3 for pie/donut chart, pull data from table sorting by the column Reason for Travel 
-   def querypieData(self):
-        # Create our session (link) from Python to the DB
+    # Query 3 for pie/donut chart - pulling data from table, sorting by the column Reason_for_Travel
+    def query_pie_data(self):
         conn = self.engine.connect()  # Raw SQL/Pandas
-        # Define Query
         query = text("""SELECT *
                         FROM touristtravel
-                        ORDER BY """)
+                        ORDER BY Reason_for_Travel""")
         df = pd.read_sql(query, con=conn)
-
-        # Close the connection
         conn.close()
-        return 
+        return
--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from flask import Flask, jsonify, render_template, redirect, request
-from SQLHelper import sqlhelper
+from SQLHelper import SQLHelper
 
 
 
@@ -9,7 +9,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # remove caching
 
 # SQL Helper
 # SQL Helper
-sqlHelper = sqlhelper()
+SQLHelper = SQLHelper()
 
 
 #################################################
@@ -43,42 +43,42 @@ def works_cited():
 @app.route("/api/v1.0/linedata/")
 def linedata():
     # Execute queries
-    df = sqlHelper.querylineData()
+    df = SQLHelper.querylineData()
     # Turn DataFrame into List of Dictionary
     data = df.to_dict(orient="records")
     return jsonify(data)
 
-# @app.route("/api/v1.0/table_data/<>")
-# def table_data(min_year):
-#     # Execute Query
-#     df = sqlHelper.queryTableData(min_year)
-#     # Turn DataFrame into List of Dictionary
-#     data = df.to_dict(orient="records")
-#     return jsonify(data)
+@app.route("/api/v1.0/bar_data/")
+def bar_data():
+    # Execute Query
+    df = SQLHelper.query_bar_data()
+    # Turn DataFrame into List of Dictionary
+    data = df.to_dict(orient="records")
+    return jsonify(data)
 
-# @app.route("/api/v1.0/map_data/<min_year>")
-# def map_data(min_year):
-#     # Execute Query
-#     df = sqlHelper.queryMapData(min_year)
-#     # Turn DataFrame into List of Dictionary
-#     data = df.to_dict(orient="records")
-#     return jsonify(data)
+@app.route("/api/v1.0/pie_data/")
+def pie_data():
+    # Execute Query
+    df = SQLHelper.query_pie_data()
+    # Turn DataFrame into List of Dictionary
+    data = df.to_dict(orient="records")
+    return jsonify(data)
 
-# #################################################
-# # ELIMINATE CACHING
-# #################################################
+#################################################
+# ELIMINATE CACHING
+#################################################
 
-# @app.after_request
-# def add_header(r):
-#     """
-#     Add headers to both force latest IE rendering engine or Chrome Frame,
-#     and also to cache the rendered page for 10 minutes.
-#     """
-#     r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
-#     r.headers["Pragma"] = "no-cache"
-#     r.headers["Expires"] = "0"
-#     return r
-# #main
-# if __name__ == "__main__":
-#     app.run(debug=True)
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
+#main
+if __name__ == "__main__":
+    app.run(debug=True)
